8dcea4b1978329677b27899f97a49660904ad2c8
Alonso Mayorga <141571699+Alonso-Mayorga@users.noreply.github.com>
2023-08-29 21:50:13 +0200

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -1,22 +1,15 @@
-# -*- coding: utf-8 -*-
 """
 Created on Tue Aug 29 20:48:12 2023
 
 @author: galgo
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug 11 11:43:50 2023
 
-@author: galgo
-
-"""
 def knapsack(values, weights, capacity):
     '''
     Function that solves the knapsack problem using dynamic programming.
     Inputs:
-        - value: List containing the value of each element
+        - values: List containing the value of each element
         - weights: List containing the weight of each element
         - capacity: Maximum capacity of the knapsack
     Outputs:
@@ -24,20 +17,19 @@ def knapsack(values, weights, capacity):
         - total_value: Total value of the elements in the knapsack
     '''
         
-    n = len(valor)
-    matriz = [[0] * (capacidad + 1) for i in range(n + 1)]
+    n = len(values)
+    matrix = [[0] * (capacity + 1) for i in range(n + 1)]
     for i in range(1, n + 1):
-        for w in range(capacidad + 1):
-            if pesos[i - 1] <= w:
-                matriz[i][w] = max(matriz[i - 1][w], matriz[i - 1][w - pesos[i - 1]] + valor[i - 1])
+        for w in range(capacity + 1):
+            if weights[i - 1] <= w:
+                matrix[i][w] = max(matrix[i - 1][w], matrix[i - 1][w - weights[i - 1]] + values[i - 1])
             else:
-                matriz[i][w] = matriz[i - 1][w]
-    valor_total = matriz[n][capacidad]
-    elementos_seleccionados = []
-    capacidad_restante = capacidad
+                matrix[i][w] = matrix[i - 1][w]
+    total_value = matrix[n][capacity]
+    selected_elements = []
+    remaining_capacity = capacity
     for i in (range(n, -1, -1)):
-        if matriz[i][capacidad_restante] != matriz[i-1][capacidad_restante]:
-            elementos_seleccionados.append(i-1)
-            capacidad_restante -= pesos[i-1]
+        if matrix[i][remaining_capacity] != matrix[i - 1][remaining_capacity]:
+            selected_elements.append(i - 1)
+            remaining_capacity -= weights[i - 1]
     return selected_elements, total_value
-
